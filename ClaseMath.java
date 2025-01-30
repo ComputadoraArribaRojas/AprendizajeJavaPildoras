9d06d26cc0095ad29cc3250a312fea8e8e1fabdb
MishiRojas <soyfito12@gmail.com>
2025-01-30 20:21:48 -0300

    primer dia

---
+++ ClaseMath.java
@@ -0,0 +1,25 @@
+import java.math.*;
+public class ClaseMath {
+
+    public static void main(String[] args) {
+
+        //double raiz = Math.sqrt(9); //Saca la raiz de un numero
+
+        double numero = 5.75;
+
+        int entero = (int) 5.52;
+
+        int resultadoRound = (int) Math.round(numero);
+
+        System.out.println(resultadoRound);
+
+        double base = 2;
+
+        double exponente = 2;
+
+        double resultadoPow = (int) Math.pow(base, exponente);
+
+        System.out.println(resultadoPow);
+    }
+
+}
