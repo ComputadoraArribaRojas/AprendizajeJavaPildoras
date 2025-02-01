170e21cfb3c3633f4d112a9db9bdbf67e56f9810
Mishi Rojas <soyfito12@gmail.com>
2025-02-01 18:30:50 -0300

    segundo commit, agregamos nuevos ejercicios

---
+++ Practicas/InvertirNumero.java
@@ -0,0 +1,20 @@
+package Practicas;
+
+import javax.swing.*;
+
+public class InvertirNumero {
+
+    public static void main(String[] args) {
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número entero positivo"));
+
+        int invertido = 0;
+
+        for (; numero != 0; numero /= 10) {
+            int digito = numero % 10;  // Extraemos el último dígito
+            invertido = invertido * 10 + digito;  // Construimos el número invertido
+        }
+
+        System.out.println("Número invertido: " + invertido);
+    }
+}
